a9225e9354d46f985ddf28f31ab5ad49ea243798
Con Meo Map Dit <yeungucnho3@gmail.com>
2023-11-22 16:12:48 +0700

    Them Reduce

---
+++ assets/js/Loops/forReduce.js
@@ -0,0 +1,55 @@
+var KamenRiders = [
+    {
+        id: "Heisei2009",
+        name: "Kiva",
+        isGood: "yes",
+        cost: 0,
+    },
+    {
+        id: "Heisei2010",
+        name: "Decade",
+        isGood: "yes",
+        cost: 10,
+
+    },
+    {
+        id: "Heisei2011",
+        name: "Double",
+        isGood: "absoluly yes",
+        cost: 30,
+
+    },
+    {
+        id: "Heisei2012",
+        name: "OOO",
+        isGood: "yes",
+        cost: 50,
+    },
+    {
+        id: "Hesei2013",
+        name: "Fourze",
+        isGood: "yes",
+        cost: 0,
+    }
+]
+
+//In
+let numOfKrNames = KamenRiders.length;
+let count = 0;
+for (let index = 0; index < numOfKrNames; ++index) {
+    console.log (count + " name: " + KamenRiders[index].name + " cost: " + KamenRiders[index].cost);
+    count++;
+}
+console.log ("");
+
+for (let index = 0; index < numOfKrNames; ++index) {
+    
+}
+console.log("");
+
+
+count=0;
+var totalKrCost = KamenRiders.reduce((total, object) => {
+    return total;
+}, typeof "ádasd")
+console.log(totalKrCost);
