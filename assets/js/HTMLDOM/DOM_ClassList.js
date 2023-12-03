e484b6d59b871e4ba2ea5a1d0dbe953bd8d2b4c0
Con Meo Map Dit <yeungucnho3@gmail.com>
2023-12-03 12:54:05 +0700

    alo alo

--- assets/js/HTMLDOM/DOM_ClassList.js
+++ assets/js/HTMLDOM/DOM_ClassList.js
@@ -28,6 +28,7 @@ console.log("");
 
 
 //add()
+console.log("Object.classList.add() => Thêm một class vào element:");
 console.log("Sử dụng classList.add(\"red\") để thêm class tên red vào.");
 boxElements.classList.add("red");
 console.log("- Lưu ý: đã viết sẵn một class tên red trong file style.css để đổi màu chữ");
@@ -35,4 +36,10 @@ console.log("");
 
 console.log("Có thể thêm vào nhiều class trong cùng một câu lệnh bằng cách classList.add(\"red\", \"blue\")");
 boxElements.classList.add("red", "blue");
+console.log("");
 
+
+//contains()
+console.log("Object.classList.contains() => Kiểm tra xem một class có tồn tại hay không");
+console.log(boxElements.classList.contains("box"));
+console.log(boxElements.classList.contains("alo"));
