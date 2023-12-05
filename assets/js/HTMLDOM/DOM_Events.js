48bed01b3d6fd66b82f4efeb517080c40afd8c13
Con Meo Map Dit <yeungucnho3@gmail.com>
2023-12-06 06:15:18 +0700

    Add DOM Events js file

--- assets/js/HTMLDOM/DOM_Events.js
+++ assets/js/HTMLDOM/DOM_Events.js
@@ -3,6 +3,6 @@
 //Assign using events
 h5Elements = document.querySelector("h5");
 
-h5Elements.onclick = (() => {
-    console.log(this);
+h5Elements.onclick = ((events) => {
+    console.log(events);
 });
