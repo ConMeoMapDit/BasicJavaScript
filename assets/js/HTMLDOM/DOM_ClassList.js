d3a7ffb5e5c98f15b2177a434ac130261f2585a3
Con meo keu meow meow <tqmtrung@gmail.com>
2023-12-05 11:28:16 +0700

    Learn InnerText

--- assets/js/HTMLDOM/DOM_ClassList.js
+++ assets/js/HTMLDOM/DOM_ClassList.js
@@ -6,40 +6,44 @@
 //remove()
 //toggle()
 
-var boxElements = document.querySelector(".box");
-console.log(boxElements);
+var boxClassList = document.querySelector(".learn-class-list");
+console.log(boxClassList);
 
-console.log(boxElements.classList);
+console.log(boxClassList.classList);
 console.log("");
 
 
-//Lenght
-console.log("Sử dụng classList.length để biết số lượng class trong element:");
-console.log(boxElements.classList.length);
-console.log("");
+//Length
+let ObjCllength = document.getElementById("class-list-length");
+let cll = document.getElementById("class-list-length").innerText;
 
-
+cll = cll + " " + ObjCllength.classList.length;
+document.getElementById("class-list-length").innerText = cll
 //value
 console.log("Sử dụng classList.value để biết tên các class trong element:");
-console.log(boxElements.classList.value);
+console.log(boxClassList.classList.value);
 console.log("- Lưu ý: classList.value trả về một chuỗi: ");
-console.log(typeof boxElements.classList.value);
+console.log(typeof boxClassList.classList.value);
 console.log("");
 
 
 //add()
 console.log("Object.classList.add() => Thêm một class vào element:");
 console.log("Sử dụng classList.add(\"red\") để thêm class tên red vào.");
-boxElements.classList.add("red");
+boxClassList.classList.add("red");
 console.log("- Lưu ý: đã viết sẵn một class tên red trong file style.css để đổi màu chữ");
 console.log("");
 
 console.log("Có thể thêm vào nhiều class trong cùng một câu lệnh bằng cách classList.add(\"red\", \"blue\")");
-boxElements.classList.add("red", "blue");
+boxClassList.classList.add("red", "blue");
 console.log("");
 
 
 //contains()
 console.log("Object.classList.contains() => Kiểm tra xem một class có tồn tại hay không");
-console.log(boxElements.classList.contains("box"));
-console.log(boxElements.classList.contains("alo"));
+console.log(boxClassList.classList.contains("box"));
+console.log(boxClassList.classList.contains("alo"));
+
+
+//remove()
+console.log("object");
