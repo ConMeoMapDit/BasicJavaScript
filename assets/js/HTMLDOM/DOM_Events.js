7d94421a3824fa993afd7f310c2999190fbd688b
Con Meo Map Dit <yeungucnho3@gmail.com>
2023-12-08 20:05:35 +0700

    Add DOM Events js file

--- assets/js/HTMLDOM/DOM_Events.js
+++ assets/js/HTMLDOM/DOM_Events.js
@@ -1,8 +1,34 @@
 // Attribute Events
 
-//Assign using events
-h5Elements = document.querySelector("h5");
-
-h5Elements.onclick = ((events) => {
+//Assign using event
+//Đây là tất cả sự kiện có thể kích hoạt của thẻ
+let getEvents = document.getElementById("show-events");
+getEvents.onclick = ((events) => {
     console.log(events);
+});
+
+console.log(getEvents.id);
+
+let eventTarget = document.getElementById("e-target");
+eventTarget.onclick = function (events) {
+    console.log(events.target);
+}
+//Sự kiện nổi bọt
+let family_arr = ["s_child"];
+let noiBot = document.getElementById("s-child");
+noiBot.onclick = ((e) => {
+    console.log(e.target);
+})
+
+let id_parent = document.getElementById("parent");
+// id_parent.onclick = function (event) {
+//     console.log(event.target );
+// }
+
+document.getElementById("uncle").onclick = ((events) => {
+    document.getElementById("uncleFriend").innerText = "Bạn của Uncle, 5s sau đi";
+
+    setTimeout(()=>{
+        document.getElementById("uncleFriend").innerText = "";
+    },5000);
 });
