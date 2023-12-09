7ace00ca437b1ebf3d5540dcb35066883416fe6f
Con meo keu meow meow <tqmtrung@gmail.com>
2023-12-09 17:13:38 +0700

    Learn InnerText

--- assets/js/HTMLDOM/DOM_Events.js
+++ assets/js/HTMLDOM/DOM_Events.js
@@ -13,22 +13,33 @@ let eventTarget = document.getElementById("e-target");
 eventTarget.onclick = function (events) {
     console.log(events.target);
 }
+
 //Sự kiện nổi bọt
 let family_arr = ["s_child"];
 let noiBot = document.getElementById("s-child");
 noiBot.onclick = ((e) => {
     console.log(e.target);
-})
+});
 
-let id_parent = document.getElementById("parent");
+// let id_parent = document.getElementById("parent");
 // id_parent.onclick = function (event) {
 //     console.log(event.target );
 // }
 
 document.getElementById("uncle").onclick = ((events) => {
-    document.getElementById("uncleFriend").innerText = "Bạn của Uncle, 5s sau đi";
+    document.getElementById("uncleFriend").innerText = "Bạn của Uncle, 3s sau đi";
 
     setTimeout(()=>{
         document.getElementById("uncleFriend").innerText = "";
-    },5000);
+    },3000);
 });
+
+
+//Truy suất đến tất cả thẻ h5
+let h5Elements = document.querySelectorAll("h5");
+let lengthH5Elements = h5Elements.length;
+for (let index = 0; index < lengthH5Elements; ++index) {
+    h5Elements[index].onclick = ((e) => {
+        console.log(e.target);
+    });
+}
